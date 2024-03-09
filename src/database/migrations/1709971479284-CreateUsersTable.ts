b5591265bb2c50aa75a8f8f02e77bc3cf80734bb
Richard <richardsgadea@gmail.com>
2024-03-09 09:14:44 +0100

    feat: Added script commando to drop and migrate tables at th same time. Created migration UsersTable.

---
+++ src/database/migrations/1709971479284-CreateUsersTable.ts
@@ -0,0 +1,69 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateUsersTable1709971479284 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "users",
+                columns:[
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment",
+                    },
+                    {
+                        name: "first_name",
+                        type: "varchar",
+                        length: "255"
+                    },
+                    {
+                        name: "last_name",
+                        type: "varchar",
+                        length: "255"
+                    },
+                    {
+                        name: "email",
+                        type: "varchar",
+                        length: "120"
+                    },
+                    {
+                        name: "password",
+                        type: "varchar",
+                        length: "120"
+                    },
+                    {
+                        name: "phone_number",
+                        type: "int",
+                    },
+                    {
+                        name: "is_active",
+                        type: "boolean",
+                        default: true,
+                    },
+                    {
+                        name: "role_id",
+                        type: "int",
+                    },
+                ],
+                foreignKeys:[
+                    {
+                        columnNames:["role_id"],
+                        referencedTableName: "roles",
+                        referencedColumnNames: ["id"],
+                    },
+                ]  
+            }),
+            true
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+
+        await queryRunner.dropTable("users");
+
+    }
+
+}

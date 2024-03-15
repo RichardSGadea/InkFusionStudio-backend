a14e44d0d23a33d8cb8b5563459df703b89cd682
Richard <richardsgadea@gmail.com>
2024-03-15 03:57:03 +0100

    feat: Added user login control route

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,23 +2,24 @@ import { Request, Response } from "express";
 import bcrypt from "bcrypt";
 import { User } from "../models/User";
 import { UserRoles } from "../constants/UserRoles";
+import jwt from "jsonwebtoken"
 
 export const authController = {
 
-    async register(req:Request,res:Response):Promise <void>{
+    async register(req: Request, res: Response): Promise<void> {
         try {
-            const {firstName, email, password} = req.body;
-    
-            if(!firstName || !email || !password){
+            const { firstName, email, password } = req.body;
+
+            if (!firstName || !email || !password) {
                 res.status(400).json({
-                    message:"All fields must be provided"
+                    message: "All fields must be provided"
                 })
                 return;
             }
 
             const users = await User.find();
-            for(let element of users){
-                if(element.email === email){
+            for (let element of users) {
+                if (element.email === email) {
                     res.status(400).json({
                         message: "Email is already in use",
                     });
@@ -26,35 +27,100 @@ export const authController = {
                 }
             };
 
-            const hashedPassword = bcrypt.hashSync(password,10);
+            const hashedPassword = bcrypt.hashSync(password, 10);
 
             const userToCreate = User.create({
-                firstName:firstName,
-                email:email,
-                password:hashedPassword,
-                role:UserRoles.CLIENT
+                firstName: firstName,
+                email: email,
+                password: hashedPassword,
+                role: UserRoles.CLIENT
             });
 
             await User.save(userToCreate);
-            
+
             res.status(201).json({
                 message: "User has been created"
             });
 
         } catch (error) {
             res.status(500).json({
-                message:"Failed to register user",
-                error: error
+                message: "Failed to register user",
 
+
+            })
+        }
+
+    },
+
+    async login(req: Request, res: Response): Promise<void> {
+
+        try {
+            const { email, password } = req.body;
+
+            if (!email || !password) {
+                res.status(400).json({
+                    message: "All fields must be provided"
+                })
+                return;
+            }
+
+            const user = await User.findOne({
+                relations: {
+                    role: true
+                },
+                where: {
+                    email: email,
+                },
+                select: {
+                    id: true,
+                    password: true,
+                    email: true,
+                }
+            })
+
+            if (!user) {
+                res.status(400).json({
+                    message: "Bad credentials"
+                })
+                return;
+            }
+
+            const isPasswordCorrect = bcrypt.compareSync(password, user.password);
+
+            if (!isPasswordCorrect) {
+                res.status(400).json({
+                    message: "Bad credentials"
+                })
+                return;
+            }
+
+            const tokenPayload = {
+                userId: user.id,
+                userRole: user.role.name,
+            }
+
+            // Generate token
+            const token = jwt.sign(
+                tokenPayload,
+                process.env.JWT_SECRET as string,
+                {
+                    expiresIn: "3h"
+                }
+            );
+
+            res.status(200).json({
+                message:"Login successfully",
+                token,
+            })
+
+        } catch (error) {
+            res.status(500).json({
+                message: "Failed to login user",
             })
         }
 
 
 
-
-    },
-
-    async login(req:Request,res:Response):Promise <void>{
 
     },
 
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -4,9 +4,7 @@ import { authController } from "../controllers/authController";
 const router = express.Router();
 
 //Authentication routes
-router.post('/login',(req, res) => {
-    res.send("Initialize");
-})
+router.post('/login',authController.login)
 router.post('/register',authController.register)
 
 export default router;

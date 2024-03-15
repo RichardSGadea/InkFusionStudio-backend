f63f864b4d03b538536f1d93e6486132ccd085c5
Richard <richardsgadea@gmail.com>
2024-03-15 04:17:28 +0100

    feat: Added middleware to authentication, and added property Request Interface to decode the token

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,38 @@
+import { de } from "@faker-js/faker";
+import {Request, Response, NextFunction } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+export const auth = (req:Request, res:Response, next: NextFunction) => {
+
+    try {
+        const token = req.headers.authorization?.split(" ")[1];
+
+        if(!token){
+            res.status(401).json({
+                message: "Unauthorized access"
+            });
+            return;
+        }
+
+        //Decoded the token to see properties
+
+        const decoded= jwt.verify(token, process.env.JWT_SECRET as string) as JwtPayload;
+
+        req.tokenData = {
+            userId: decoded.userId,
+            userRole: decoded.userRole,
+        }
+
+
+        next();
+
+
+    } catch (error) {
+        res.status(401).json({
+            message:"Invalid token provided"
+        })
+    }
+
+
+
+}
